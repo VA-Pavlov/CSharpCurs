096dddb09a4b55d35e1a40d3d675c294d892475d
Pavlov-VA <vap.uspeg@gmail.com>
2024-01-17 14:48:17 +0300

    number 1

--- CSharpCurs/Program.cs
+++ CSharpCurs/Program.cs
@@ -10,6 +10,41 @@ namespace CSharpCurs
     {
         static void Main(string[] args)
         {
+            Exercise1();
         }
-    }
+
+        static void Exercise1()
+        {
+            Random random = new Random();
+            int ArraySize = random.Next(5, 20);
+            int[] MyArray = new int[ArraySize];
+            Console.Write("array = {");
+            for(int i = 0; i < ArraySize;i++)
+            {
+                MyArray[i] = random.Next(-100, 100);
+                Console.Write(MyArray[i]+" ");
+            }
+            Console.WriteLine("}");
+
+            Array.Sort(MyArray);
+            int Counter = 0;
+            for(int i = 0;i < ArraySize;i++)
+            {
+                while (i != ArraySize - 1 && MyArray[i] == MyArray[i + 1]  )
+                {
+                    i++;
+                }
+                Counter++;
+
+            }
+            
+            Console.WriteLine(ArraySize +" "+ Counter);
+        }
+
+        static void Exercise2()
+        {
+
+        }
+
+        }
 }
